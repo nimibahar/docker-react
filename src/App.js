d9bdab17f9e4e100ceab4f7bcc27e862e08781e8
nimrod bahar <nimrod.bahar@coinmama.com>
2019-01-03 14:18:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn some cool React with docker ohhhhh yeaaa
           </a>
         </header>
       </div>
